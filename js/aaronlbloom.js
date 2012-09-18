12cd18e767ed32079d23add66b9b20fa2d827b07
Aaron Bloom <aaronlbloom@gmail.com>
2012-09-18 16:58:32 -0400

    New background, . Simpler layout
    New background, . Simpler layout

--- js/aaronlbloom.js
+++ js/aaronlbloom.js
@@ -1,6 +1,6 @@
 <!-- Draggable box  from: http://jqueryui.com/demos/draggable/ , Dialog Box from http://jqueryui.com/demos/dialog/#animated-->
 				$(function() {
-					$( "#draggable1" ).draggable();
+					$( "#draggable" ).draggable();
 				});
 					// increase the default animation speed to exaggerate the effect
 					$.fx.speeds._default = 1000;
@@ -15,17 +15,3 @@
 							return false;
 						});
 					});			
-						$(function() {
-					$( "#draggable2" ).draggable();
-				});
-					<!--Thanks to the following for the hide/show trick: http://woork.blogspot.com/2007/10/show-hide-layer-using-simple-javascript.html-->
-					function showlayer(layer){
-									if(layer=="classCard"){
-										document.getElementById("classCard").style.visibility='visible';
-									    document.getElementById("mainCard").style.visibility='hidden';
-									} else { 
-										document.getElementById("mainCard").style.visibility='visible';
-									    document.getElementById("classCard").style.visibility='hidden';
-									
-							}
-					}
