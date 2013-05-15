f4db5028ba319cc6a1e8b481a2cea5d86dce367f
Aaron Bloom <aaronlbloom@gmail.com>
2013-05-15 17:16:26 -0400

    fix main page, add work/life

--- js/aaronlbloom.js
+++ js/aaronlbloom.js
@@ -1,7 +1,10 @@
 <!-- Draggable box  from: http://jqueryui.com/demos/draggable/ , Dialog Box from http://jqueryui.com/demos/dialog/#animated-->
-				$(function() {
+
+$(document).ready(function(){ 
+					$(function() {
 					$( "#draggable" ).draggable();
 				});
+			
 					// increase the default animation speed to exaggerate the effect
 					$.fx.speeds._default = 1000;
 					$(function() {
@@ -15,3 +18,18 @@
 							return false;
 						});
 					});			
+$("#homeButton").click(function() {
+	console.log('Testing console');	  
+      	  	  $( "#home" ).css("display","block");
+      	      $( "#work" ).css("display","none");
+	  return false;
+        });
+        
+$("#workButton").click(function() {
+      	  console.log('Testing console');	  
+      	  	  $( "#home" ).css("display","none");
+      	      $( "#work" ).css("display","block");
+	  return false;
+        });
+        
+ }); 
